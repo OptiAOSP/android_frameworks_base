0f91b5106c8cbb80e86519ea527cb2a5e094d369
maxwen <max.weninger@gmail.com>
2015-02-19 15:15:49 -0800

    Add OmniSwitch as option for default recents [1/2]
    I don't know who the original author of this commit is, but they deserve the credit
    
    Change-Id: I6e7e1a493ab4f96abc13439466b63ee9ad1b8e5f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4031,6 +4031,12 @@ public final class Settings {
         public static final String RECENTS_SEARCH_BAR = "recents_search_bar";
 
         /**
+         * Alternative recent apps integration using OmniSwitch
+         * @hide
+         */
+        public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
