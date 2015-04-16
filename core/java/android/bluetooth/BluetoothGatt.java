af9f0f1faa6bdf1f0277ac7ba3b0f0801361352a
Sungki Kim <skkim7@gmail.com>
2015-04-17 02:05:19 +0900

    fix race condition.
    Race codition occurs after call registerApp().
    ("mAutoConnect = autoConnect;" and call mService.clientConnect(... mAutoConnect ) in onClientRegistered())
    So "mAutoConnect = autoConnect;" move to front.
    
    Change-Id: Ie1174c0f224f5084178439420b383164d22d542c

--- core/java/android/bluetooth/BluetoothGatt.java
+++ core/java/android/bluetooth/BluetoothGatt.java
@@ -734,6 +734,7 @@ public final class BluetoothGatt implements BluetoothProfile {
             }
             mConnState = CONN_STATE_CONNECTING;
         }
+        mAutoConnect = autoConnect;
         if (!registerApp(callback)) {
             synchronized(mStateLock) {
                 mConnState = CONN_STATE_IDLE;
@@ -743,7 +744,6 @@ public final class BluetoothGatt implements BluetoothProfile {
         }
 
         // the connection will continue after successful callback registration
-        mAutoConnect = autoConnect;
         return true;
     }
 
