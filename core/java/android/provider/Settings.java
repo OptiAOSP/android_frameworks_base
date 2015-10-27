35801455a8f0385baf3c4ec4b6d7623836421213
Myself5 <myself5@carbonrom.org>
2015-10-27 21:03:42 +0100

    fwb: Add ability to permanently hide apps from recents [1/2]
    This is based off XposedAppSettings, but modified to fit our needs, and
    a little bit of: d9973ba0ef48d27688df1e6b33a9777b67b24142
    
    Change-Id: Id1a599df90f3c12ccd32d69eb2e262a5d8f464ab

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4121,6 +4121,12 @@ public final class Settings {
         public static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
 
         /**
+         *  Used to block apps from showing in recents
+         * @hide
+         */
+        public static final String BLOCKED_BY_RECENTS = "blocked_by_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
