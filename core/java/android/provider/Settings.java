e4cfd67f6be7ee420a6396266159666716ca689c
maxwen <max.weninger@windriver.com>
2015-12-21 16:35:23 +0100

    base: weather in header view [1/2]
    Change-Id: I4087bb5de900f0846d5756768d43eb32f293457c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3983,6 +3983,18 @@ public final class Settings {
         public static final String COLOR_PICKER_PALETTE = "color_picker_palette";
 
         /**
+         * Enable weather display in header view
+         * @hide
+         */
+        public static final String STATUS_BAR_HEADER_WEATHER = "status_bar_header_weather";
+
+        /**
+         * icon pack to use for weather condition images
+         * @hide
+         */
+        public static final String STATUS_BAR_WEATHER_ICON_PACK = "status_bar_weather_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
