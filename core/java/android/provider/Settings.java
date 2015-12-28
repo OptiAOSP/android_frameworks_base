76693cc897bbdc6606e70c78e7c3d397e9e0402f
Stevespear426 <stephen.k.spear@gmail.com>
2015-12-28 19:04:27 +0100

    FWB: More system animations improvements (1/2)
    Brought back from kitkat by @LorDClockaN
    
    Change-Id: I7974c117c46adb8210d357434912a2cf15fe3204

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4181,6 +4181,12 @@ public final class Settings {
          */
         public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
 
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
+        public static final String ANIMATION_CONTROLS_EXIT_ONLY = "animation_controls_exit_only";
+
+        public static final String ANIMATION_CONTROLS_REVERSE_EXIT = "animation_controls_reverse_exit";
+
         /**
          * @hide
          */
