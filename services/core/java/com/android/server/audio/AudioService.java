76679a936d11dc134dd1062e9d86c64f8cf6dec6
B--B <dark.schneider.ds@gmail.com>
2016-01-02 12:31:36 +0100

    Revert "Reduce the delay during headset removal"
    This reverts commit d532a9a3e53d1b79b81ecb4431be5f773c8f404c.

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -4777,7 +4777,7 @@ public class AudioService extends IAudioService.Stub {
                         0,
                         null,
                         0);
-                delay = 700;
+                delay = 1000;
             }
         }
 
