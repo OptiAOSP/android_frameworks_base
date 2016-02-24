cac6e6ccff29d82e3dca19cbff6ede383cbd456d
Takaki Makino <tmakino@google.com>
2016-02-24 15:34:14 +0900

    Stop adjustConfigurationLw causing sleep/lock.
    Currently PhoneWindowManager.adjustConfigurationLw
    may trigger device sleep or screen lock when lid is closed.
    adjustConfigurationLw is not expected to make a state change, since
    the only caller is WindowManagerService.computeScreenConfigurationLocked,
    whose role is only to compute configuration.
    Closed lid event is handled in PhoneWindowManager.notifyLidSwitchChanged,
    so we don't need the call to applyLidSwitchState here.
    
    Change-Id: Ia39db96d3e19f71cbad571571a432011b9422a72
    Signed-off-by: Takaki Makino <tmakino@google.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2217,7 +2217,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         readConfigurationDependentBehaviors();
         readLidState();
-        applyLidSwitchState();
 
         if (config.keyboard == Configuration.KEYBOARD_NOKEYS
                 || (keyboardPresence == PRESENCE_INTERNAL
