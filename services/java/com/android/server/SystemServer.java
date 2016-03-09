e13df4eada37935ad9076f45bd4a80b8d5e0ef1c
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-10 03:56:03 +0700

    Revert "frameworks-base_SystemServer-remove-unused-first-boot-flag.patch"
    This reverts commit 4db04ee61e9d6cfa1a009fb8e074bd380c527161.
    
    Change-Id: I308182d275298bb9c89579cae506435385e8ef00

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -158,6 +158,7 @@ public final class SystemServer {
     private ContentResolver mContentResolver;
 
     private boolean mOnlyCore;
+    private boolean mFirstBoot;
 
     private class AdbPortObserver extends ContentObserver {
         public AdbPortObserver() {
@@ -362,6 +363,7 @@ public final class SystemServer {
         Slog.i(TAG, "Package Manager");
         mPackageManagerService = PackageManagerService.main(mSystemContext, installer,
                 mFactoryTestMode != FactoryTest.FACTORY_TEST_OFF, mOnlyCore);
+        mFirstBoot = mPackageManagerService.isFirstBoot();
         mPackageManager = mSystemContext.getPackageManager();
 
         Slog.i(TAG, "User Service");
