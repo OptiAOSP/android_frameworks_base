129888792126cdbef6d3a06c2ef3da34c7d53ad2
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-13 04:03:22 +0700

    hwui: fix screen flickering

--- libs/hwui/renderthread/RenderProxy.cpp
+++ libs/hwui/renderthread/RenderProxy.cpp
@@ -142,7 +142,7 @@ void RenderProxy::initialize(const sp<Surface>& surface) {
     SETUP_TASK(initialize);
     args->context = mContext;
     args->surface = surface.get();
-    post(task);
+    postAndWait(task);
 }
 
 CREATE_BRIDGE2(updateSurface, CanvasContext* context, Surface* surface) {
