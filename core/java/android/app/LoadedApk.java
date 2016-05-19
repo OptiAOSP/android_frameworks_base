SphinxOS/frameworks_base

Make fake libart for misbehaving apps be loaded.
Bug: 27775991
Change-Id: Ie4eba211e876ec0643ce6ecf6a110f3cb5e0f57e

Former-commit-id: 20ed24fc0cbbada36e84dc1f1e86946db6e6c5a4

core/java/android/app/LoadedApk.java
@@ -372,6 +372,11 @@ public final class LoadedApk {
             // Add path to libraries in apk for current abi. Do this now because more entries
             // will be added to zipPaths that shouldn't be part of the library path.
             if (aInfo.primaryCpuAbi != null) {
+                // Add fake libs into the library search path if we target prior to N.
+                if (aInfo.targetSdkVersion <= 23) {
+                    outLibPaths.add("/system/fake-libs" +
+                        (VMRuntime.is64BitAbi(aInfo.primaryCpuAbi) ? "64" : ""));
+                }
                 for (String apk : outZipPaths) {
                     outLibPaths.add(apk + "!/lib/" + aInfo.primaryCpuAbi);
                 }

OptiAOSP/android_frameworks_base

(cherry picked from commit 20ed24fc0cbbada36e84dc1f1e86946db6e6c5a4)

core/java/android/app/LoadedApk.java
@@ -356,7 +356,12 @@ public final class LoadedApk {
                 if (mApplicationInfo.primaryCpuAbi != null) {
                     for (String apk : apkPaths) {
                       libPaths.add(apk + "!/lib/" + mApplicationInfo.primaryCpuAbi);
-                    }
+                      // Add fake libs into the library search path if we target prior to N.
+                      if (mApplicationInfo.targetSdkVersion <= 23) {
+                          libPaths.add("/system/fake-libs" +
+                              (VMRuntime.is64BitAbi(mApplicationInfo.primaryCpuAbi) ? "64" : ""));
+                      }
+                   }
                 }
 
                 final String lib = TextUtils.join(File.pathSeparator, libPaths);