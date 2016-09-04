AOSPA/android_frameworks_base-old

Add two fixes for MuiltiSim support
Use getDefaultDataSubId to get default data network type. Currently using
getDefaultSubscription() may return voice network subId.

Provide database flags per subscription specific for both mobile data
and roaming options.

CRs-Fixed: 963773
Change-Id: I1d40bc5c84b4428d7066b28003e5b2c5d852b6f7

telephony/java/android/telephony/TelephonyManager.java
@@ -1450,7 +1450,7 @@ public class TelephonyManager {
      * @hide
      */
     public int getDataNetworkType() {
-        return getDataNetworkType(getDefaultSubscription());
+        return getDataNetworkType(getDefaultDataSubscription());
     }
 
     /**
@@ -3261,6 +3261,13 @@ public class TelephonyManager {
     }
 
     /**
+     * Returns Default Data subscription.
+     */
+    private static int getDefaultDataSubscription() {
+        return SubscriptionManager.getDefaultDataSubId();
+    }
+
+    /**
      * Returns Default phone.
      */
     private static int getDefaultPhone() {
@@ -4218,7 +4225,7 @@ public class TelephonyManager {
     /** @hide */
     @SystemApi
     public void setDataEnabled(boolean enable) {
-        setDataEnabled(SubscriptionManager.getDefaultDataSubId(), enable);
+        setDataEnabled(getDefaultDataSubscription(), enable);
     }
 
     /** @hide */
@@ -4237,7 +4244,7 @@ public class TelephonyManager {
     /** @hide */
     @SystemApi
     public boolean getDataEnabled() {
-        return getDataEnabled(SubscriptionManager.getDefaultDataSubId());
+        return getDataEnabled(getDefaultDataSubscription());
     }
 
     /** @hide */

OptiAOSP/android_frameworks_base

telephony/java/android/telephony/TelephonyManager.java
@@ -1548,7 +1548,7 @@ public class TelephonyManager {
      *   {@link android.Manifest.permission#READ_PHONE_STATE READ_PHONE_STATE}
      */
     public int getDataNetworkType() {
-        return getDataNetworkType(getSubId());
+        return getDataNetworkType(getDefaultDataSubscriptionId());
     }
 
     /**
@@ -3478,6 +3478,13 @@ public class TelephonyManager {
     }
 
     /**
+     * Returns Default Data subscription.
+     */
+    private static int getDefaultDataSubscriptionId() {
+        return SubscriptionManager.getDefaultDataSubscriptionId();
+    }
+
+    /**
      * Returns Default phone.
      */
     private static int getDefaultPhone() {
@@ -4557,7 +4564,7 @@ public class TelephonyManager {
     /** @hide */
     @SystemApi
     public void setDataEnabled(boolean enable) {
-        setDataEnabled(SubscriptionManager.getDefaultDataSubscriptionId(), enable);
+        setDataEnabled(getDefaultDataSubscriptionId(), enable);
     }
 
     /** @hide */
@@ -4576,7 +4583,7 @@ public class TelephonyManager {
     /** @hide */
     @SystemApi
     public boolean getDataEnabled() {
-        return getDataEnabled(SubscriptionManager.getDefaultDataSubscriptionId());
+        return getDataEnabled(getDefaultDataSubscriptionId());
     }
 
     /** @hide */