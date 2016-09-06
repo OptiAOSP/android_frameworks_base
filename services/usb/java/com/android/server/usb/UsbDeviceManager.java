0cb2e99bf75e780ce4b7e0da6dbaee3bef307806
c_yunong <yunong@codeaurora.org>
2016-09-06 15:02:00 +0800

    Update the default usb config
    Enable the data transfer when the default usb mode is mtp or ptp.
    
    Change-Id: I12c662876ee8dfc7caab8a29fced62c06fdd2525
    CRs-fixed: 1056112

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -356,7 +356,16 @@ public class UsbDeviceManager {
                         UsbManager.USB_FUNCTION_ADB);
 
                 setEnabledFunctions(null, false, false);
-
+                if (mContext.getResources().getBoolean(
+                        com.android.internal.R.bool.config_usb_data_unlock)) {
+                    boolean mtpEnable = UsbManager.containsFunction(getDefaultFunctions(),
+                            UsbManager.USB_FUNCTION_MTP);
+                    boolean ptpEnable = UsbManager.containsFunction(getDefaultFunctions(),
+                            UsbManager.USB_FUNCTION_PTP);
+                    boolean umsEnable = UsbManager.containsFunction(getDefaultFunctions(),
+                            UsbManager.USB_FUNCTION_UMS);
+                    if (mtpEnable || ptpEnable) mUsbDataUnlocked = true;
+                }
                 String state = FileUtils.readTextFile(new File(STATE_PATH), 0, null).trim();
                 updateState(state);
 
