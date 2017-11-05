d406a4031584454becdc58e873b8c1facbae679f
Shilin Victor <chrono.monochrome@gmail.com>
2017-11-05 15:22:05 +0300

    app_process: set wider dalvik-cache permissions
    Change-Id: I9d88d0eb8c136b241542425d5ad8fa03f3d5b9ba

--- cmds/app_process/app_main.cpp
+++ cmds/app_process/app_main.cpp
@@ -161,17 +161,14 @@ static void maybeCreateDalvikCache() {
     LOG_ALWAYS_FATAL_IF((numChars >= PATH_MAX || numChars < 0),
             "Error constructing dalvik cache : %s", strerror(errno));
 
-    int result = mkdir(dalvikCacheDir, 0711);
+    int result = mkdir(dalvikCacheDir, 0777);
     LOG_ALWAYS_FATAL_IF((result < 0 && errno != EEXIST),
             "Error creating cache dir %s : %s", dalvikCacheDir, strerror(errno));
 
-    // We always perform these steps because the directory might
-    // already exist, with wider permissions and a different owner
-    // than we'd like.
-    result = chown(dalvikCacheDir, AID_ROOT, AID_ROOT);
+    result = chown(dalvikCacheDir, AID_SYSTEM, AID_SYSTEM);
     LOG_ALWAYS_FATAL_IF((result < 0), "Error changing dalvik-cache ownership : %s", strerror(errno));
 
-    result = chmod(dalvikCacheDir, 0711);
+    result = chmod(dalvikCacheDir, 0777);
     LOG_ALWAYS_FATAL_IF((result < 0),
             "Error changing dalvik-cache permissions : %s", strerror(errno));
 }
