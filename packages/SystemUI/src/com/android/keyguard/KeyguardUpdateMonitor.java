e82301e731cc314179c97c4a2ad58708563c1a68
Shilin Victor <chrono.monochrome@gmail.com>
2017-11-05 23:48:23 +0300

    SystemUI: RIL API fixes
    Change-Id: I75d85520c2dcc851badc8f490684fd068430aad2

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1609,7 +1609,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
      */
     public void reportSimUnlocked(int subId) {
         if (DEBUG_SIM_STATES) Log.v(TAG, "reportSimUnlocked(subId=" + subId + ")");
-        int slotId = SubscriptionManager.getSlotIndex(subId);
+        int slotId = SubscriptionManager.getSlotId(subId);
         handleSimStateChange(subId, slotId, State.READY);
     }
 
@@ -1778,7 +1778,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         for (int i = 0; i < list.size(); i++) {
             final SubscriptionInfo info = list.get(i);
             final int id = info.getSubscriptionId();
-            int slotId = SubscriptionManager.getSlotIndex(id);
+            int slotId = SubscriptionManager.getSlotId(id);
             if (state == getSimState(id) && bestSlotId > slotId ) {
                 resultId = id;
                 bestSlotId = slotId;
