ec7a4354ab33be8127e44357a736d99dd0830403
Shilin Victor <chrono.monochrome@gmail.com>
2018-01-14 18:07:58 +0300

    RemoteViews: Fix "Could not copy bitmap to parcel blob"
    Limit the size of mBitmapCache's mBitmaps, to prevent
    opening too many fds when transfering between processes
    by Parcel.
    
    Change-Id: I77ca5f6d49642f2fa8c9d729ec3e0bf9a0cff486

--- core/java/android/widget/RemoteViews.java
+++ core/java/android/widget/RemoteViews.java
@@ -54,6 +54,7 @@ import android.os.UserHandle;
 import android.text.TextUtils;
 import android.util.ArrayMap;
 import android.util.Log;
+import android.util.Slog;
 import android.view.LayoutInflater;
 import android.view.LayoutInflater.Filter;
 import android.view.RemotableViewMethod;
@@ -1163,7 +1164,10 @@ public class RemoteViews implements Parcelable, Filter {
     }
 
     private static class BitmapCache {
+        static final int MAX_CACHE_BITMAPS = 50;
+
         ArrayList<Bitmap> mBitmaps;
+        int mCount;
 
         public BitmapCache() {
             mBitmaps = new ArrayList<Bitmap>();
@@ -1174,7 +1178,22 @@ public class RemoteViews implements Parcelable, Filter {
             mBitmaps = new ArrayList<Bitmap>();
             for (int i = 0; i < count; i++) {
                 Bitmap b = Bitmap.CREATOR.createFromParcel(source);
+                addBitmap(b);
+            }
+        }
+
+        private int addBitmap(Bitmap b) {
+            mCount ++;
+            if (mCount > MAX_CACHE_BITMAPS) {
+                int index = (mCount - 1) % MAX_CACHE_BITMAPS;
+                Slog.w(LOG_TAG, "RemoteViews try to cache " + mCount
+                        + " bitmaps, only allows " + MAX_CACHE_BITMAPS
+                        + ", replace bitmap at index " + index);
+                mBitmaps.set(index, b);
+                return index;
+            } else {
                 mBitmaps.add(b);
+                return (mCount - 1);
             }
         }
 
@@ -1185,8 +1204,7 @@ public class RemoteViews implements Parcelable, Filter {
                 if (mBitmaps.contains(b)) {
                     return mBitmaps.indexOf(b);
                 } else {
-                    mBitmaps.add(b);
-                    return (mBitmaps.size() - 1);
+                    return addBitmap(b);
                 }
             }
         }
@@ -1213,7 +1231,7 @@ public class RemoteViews implements Parcelable, Filter {
             for (int i = 0; i < count; i++) {
                 Bitmap b = bitmapsToBeAdded.get(i);
                 if (!mBitmaps.contains(b)) {
-                    mBitmaps.add(b);
+                    addBitmap(b);
                 }
             }
         }
